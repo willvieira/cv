57ae411db881be7da713741d215eb048cbe7f2c8
Willian <willian.vieira@usherbrooke.ca>
2020-07-23 11:14:32 -0400

    ✨ add preloader animation

---
+++ assets/preloader.js
@@ -0,0 +1,12 @@
+<script>
+
+  window.onload = function() {
+      setTimeout(
+        function() {
+          document.getElementById("spinner").style.display = "none";
+          document.getElementById("overlay").style.display = "none";
+
+        }, 2100)
+  }
+
+</script>
