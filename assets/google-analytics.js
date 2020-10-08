080348cbcf35b8c699d3e9a75fb108286e4baefc
Willian <willian.vieira@usherbrooke.ca>
2020-10-08 12:07:13 -0400

    ✨ gg analytics

---
+++ assets/google-analytics.js
@@ -0,0 +1,9 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-90532848-7">
+</script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-90532848-7');
+</script>
